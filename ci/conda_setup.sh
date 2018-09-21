a5721f6e45a1e12f43058f1cb22258c0ada8b4ed
lext <aleksei.tiulpin@protonmail.ch>
2018-09-21 22:00:11 +0300

    conda CI

--- ci/conda_setup.sh
+++ ci/conda_setup.sh
@@ -1,11 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 
-if [ "`uname`" == "Darwin" ];
+if [ $TRAVIS_OS_NAME == "osx" ];
 then
+    echo "Getting Conda for OSX...\n"
     wget https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O $HOME/miniconda.sh;
 fi
 
-if [ "`uname`" == "Linux" ];
+if [ $TRAVIS_OS_NAME == "linux" ];
 then
+    echo "Getting Conda for Linux...\n"
     wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O $HOME/miniconda.sh;
 fi
