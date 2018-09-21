f42a15162b6cfbc739bf0bef8a5c78f0cacc5c5f
lext <aleksei.tiulpin@protonmail.ch>
2018-09-21 21:55:40 +0300

    more improvements

--- ci/conda_setup.sh
+++ ci/conda_setup.sh
@@ -2,10 +2,10 @@
 
 if [ "`uname`" == "Darwin" ];
 then
-    wget https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O miniconda.sh;
+    wget https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O $HOME/miniconda.sh;
 fi
 
 if [ "`uname`" == "Linux" ];
 then
-    wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
+    wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O $HOME/miniconda.sh;
 fi
