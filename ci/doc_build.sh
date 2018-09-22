87286edb1a4d733326aecc4ed342e0d7043d118b
lext <aleksei.tiulpin@protonmail.ch>
2018-09-22 12:26:33 +0300

    docs deploy

--- ci/doc_build.sh
+++ ci/doc_build.sh
@@ -2,5 +2,6 @@
 
 if [ $TRAVIS_PYTHON_VERSION = 3.7 ] && [ $TRAVIS_OS_NAME = "linux" ];
 then
-    travis-sphinx build --source=./doc/source/ && travis-sphinx deploy
+    travis-sphinx build --source=./doc/source/
+    travis-sphinx deploy
 fi
