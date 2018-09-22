51aa0fb23143570f4b76223e4c1d4ca77195e316
lext <aleksei.tiulpin@protonmail.ch>
2018-09-22 12:17:23 +0300

    docs if statement

--- ci/doc_build.sh
+++ ci/doc_build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [[ $TRAVIS_PYTHON_VERSION = 3.7 &&  $TRAVIS_OS_NAME = "linux" ]];
+if [ $TRAVIS_PYTHON_VERSION = 3.7 ] && [ $TRAVIS_OS_NAME = "linux" ];
 then
     travis-sphinx build --source=./doc/source/ && travis-sphinx deploy
 fi
