0f556100d9157c5f92b1fd313671abbade10b2ac
lext <aleksei.tiulpin@protonmail.ch>
2018-09-22 11:27:07 +0300

    conditions in the doc build script

--- ci/doc_build.sh
+++ ci/doc_build.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-travis-sphinx build --source=./doc/source/ && travis-sphinx deploy
+if [[ $TRAVIS_PYTHON_VERSION = 3.7 &&  $TRAVIS_OS_NAME = "linux" ]];
+then
+    travis-sphinx build --source=./doc/source/ && travis-sphinx deploy
+fi
