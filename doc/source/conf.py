96210e2eca1aa93b1abfa51ed4132540761b0f65
lext <aleksei.tiulpin@protonmail.ch>
2019-07-26 11:16:03 +0300

    Docs changes

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -27,7 +27,7 @@ author = 'Aleksei Tiulpin'
 # The short X.Y version
 version = 'v0.1'
 # The full version, including alpha/beta/rc tags
-release = 'v0.1.7'
+release = 'v0.1.8'
 
 
 # -- General configuration ---------------------------------------------------
