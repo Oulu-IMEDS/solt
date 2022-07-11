171f2c119db45c7461931a57f5f7916ccbf130fa
Aleksei Tiulpin <aleksei.tiulpin@protonmail.ch>
2022-07-11 11:50:49 +0300

    random resized crop is now fixed in docs

--- solt/transforms/__init__.py
+++ solt/transforms/__init__.py
@@ -22,6 +22,7 @@ from ._transforms import (
     KeypointsJitter,
     JPEGCompression,
     GridMask,
+    RandomResizedCrop
 )
 
 __all__ = [
@@ -48,4 +49,5 @@ __all__ = [
     "KeypointsJitter",
     "JPEGCompression",
     "GridMask",
+    "RandomResizedCrop"
 ]
